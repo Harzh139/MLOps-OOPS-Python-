099b9f18bc356c9819fcf9c7ba7461eeafbf62ff
harsh <sharmaharshu139@gmail.com>
2025-11-26 15:07:26 +0530

    all updated

--- oops1.py
+++ oops1.py
@@ -2,22 +2,32 @@
 class employee:
     #special function/method/dunder method -constructor
     def __init__(self):
-        print(id(self))
+        self.__name = "Default"
         #print("Started data fetching....")
         self.id = 123
         self.salary = 50000 
         self.designation = "software development engineer"
        # print("Data fetched!")
 
+    def get_name(self):
+        return self.__name
+    
+    def set_name(self,value):
+        self.__name = value
+
     def travel(self,destination):
         print("This travel function was called manually")
         print(f'Employee is now traveling to {destination}')
 
 #creating an object/instance of class employee
-sam = employee()
-print(id(sam))
+#sam = employee()
+#print(id(sam))
 
 #print(sam.salary)
 #sam.travel("Delhi")
 
 #print(type(sam))
+user1 = employee()
+print(user1.get_name())
+user1.set_name("Agent X")
+print(user1.get_name())
